de6b6f320b597c43702506efc817b8af81582bca
Amanda Surber <surberaleigh@gmail.com>
2020-03-26 00:27:31 -0400

    adding db and tables in schema file

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeetrackerdb;
+
+CREATE database employeetrackerdb;
+
+USE employeetrackerdb;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL auto_increment PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER
+);
